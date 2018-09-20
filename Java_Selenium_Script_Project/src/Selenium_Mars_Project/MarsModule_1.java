8a30f00755e662cb1ce4c75355cb181ce6075932
vasan <vasan@10.0.0.52>
2018-09-20 18:26:01 -0400

    This is the initial project commit to local repository
    Signed-off-by: vasan <vasan@10.0.0.52>

---
+++ Java_Selenium_Script_Project/src/Selenium_Mars_Project/MarsModule_1.java
@@ -0,0 +1,10 @@
+package Selenium_Mars_Project;
+
+public class MarsModule_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("This is the first module of selenium mars project");
+	}
+
+}
